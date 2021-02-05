44b12434fb5c3b8d0947c8235d3e23b64264e600
Farzan-ul-haq <57554503+Farzan-ul-haq@users.noreply.github.com>
2021-02-06 00:53:16 +0500

    Initial Structure

---
+++ resizable_array.py
@@ -0,0 +1,26 @@
+
+class ResizableArray:
+    def __init__(self, size):
+        self.size = size
+        self.capacity = 0
+        self.array = [None for i in range(self.size)]
+        # self.
+
+    
+    def insert(self, index, value):
+        pass
+
+    def update(self, index, value):
+        pass
+
+    def get(self, index):
+        pass
+    
+    def remove(self, index):
+        pass
+
+    def printarray(self):
+        return self.array
+
+    def traverse(self):
+        pass
