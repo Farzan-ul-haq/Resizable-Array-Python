bf0583735e0f390126fe2489935e4660baa80bbb
Muhammad Hammad <hammadarshad1@gmail.com>
2021-02-06 19:44:01 +0500

    comments added

--- resizable_array.py
+++ resizable_array.py
@@ -2,25 +2,44 @@
 class ResizableArray:
     def __init__(self, size):
         self.size = size
-        self.capacity = 0
+        self.no_of_elements = 0
         self.array = [None for i in range(self.size)]
         # self.
 
     
     def insert(self, index, value):
-        pass
+        '''
+        Insert the value at the given index of an array
+        '''
+        if index > self.no_of_elements:
 
     def update(self, index, value):
-        pass
+        '''
+        Update the value at the given index of an array
+        '''
 
     def get(self, index):
-        pass
+        '''
+        Returns the value from the given index
+        '''
     
     def remove(self, index):
-        pass
+        '''
+        Remove the value from the given index
+        '''
 
     def printarray(self):
+        '''
+        print the whole array
+        '''
         return self.array
 
     def traverse(self):
-        pass
+        '''
+        Print all the values of an array
+        '''
+
+    def resize(self):
+        '''
+        Resize/shrink the array
+        '''
